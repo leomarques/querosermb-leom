f5f8e1bc24d1e578b3c2ed1b38f1fbd333f86d26
Leonardo Marques <leommarques7@gmail.com>
2025-03-26 16:49:33 -0300

    style: change details layout

--- app/src/main/java/com/querosermb/leom/details/DetailsLayout.kt
+++ app/src/main/java/com/querosermb/leom/details/DetailsLayout.kt
@@ -1,6 +1,7 @@
 package com.querosermb.leom.details
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -15,12 +16,25 @@ fun DetailsLayout(
     exchange: Exchange,
     modifier: Modifier = Modifier
 ) {
-    Column(modifier = modifier.padding(16.dp)) {
+    Column(
+        modifier =
+            modifier
+                .fillMaxSize()
+                .padding(16.dp)
+    ) {
         Text(text = "Nome: ${exchange.name}")
         Text(text = "ID: ${exchange.exchangeId}")
-        Text(text = "Volume em USD em 1 dia: ${exchange.volume1dayUsd}")
-        Text(text = "Rank: ${exchange.rank}")
         Text(text = "website: ${exchange.website}")
+        Text(text = "Rank: ${exchange.rank}")
+        Text(text = "Volume em USD em 1 hora: ${exchange.volume1hrsUsd}")
+        Text(text = "Volume em USD em 1 dia: ${exchange.volume1dayUsd}")
+        Text(text = "Volume em USD em 1 mês: ${exchange.volume1mthUsd}")
+        Text(text = "Orderbook start: ${exchange.dataOrderbookStart}")
+        Text(text = "Orderbook end: ${exchange.dataOrderbookEnd}")
+        Text(text = "Trade start: ${exchange.dataTradeStart}")
+        Text(text = "Trade end: ${exchange.dataTradeEnd}")
+        Text(text = "Quote start: ${exchange.dataQuoteStart}")
+        Text(text = "Quote end: ${exchange.dataQuoteEnd}")
     }
 }
 
--- domain/src/main/java/com/querosermb/domain/model/ExchangeExts.kt
+++ domain/src/main/java/com/querosermb/domain/model/ExchangeExts.kt
@@ -2,24 +2,8 @@ package com.querosermb.domain.model
 
 import com.querosermb.data.http.ExchangeAPIModel
 import com.querosermb.data.local.ExchangeEntity
-
-fun ExchangeEntity.toDomain() =
-    Exchange(
-        exchangeId = exchangeId,
-        website = website,
-        name = name,
-        dataQuoteStart = dataQuoteStart,
-        dataQuoteEnd = dataQuoteEnd,
-        dataOrderbookStart = dataOrderbookStart,
-        dataOrderbookEnd = dataOrderbookEnd,
-        dataTradeStart = dataTradeStart,
-        dataTradeEnd = dataTradeEnd,
-        dataSymbolsCount = dataSymbolsCount,
-        volume1hrsUsd = volume1hrsUsd,
-        volume1dayUsd = volume1dayUsd,
-        volume1mthUsd = volume1mthUsd,
-        rank = rank,
-    )
+import java.time.OffsetDateTime
+import java.time.format.DateTimeFormatter
 
 fun ExchangeAPIModel.toExchangeEntity() = ExchangeEntity(
     exchangeId = exchangeId,
@@ -37,3 +21,28 @@ fun ExchangeAPIModel.toExchangeEntity() = ExchangeEntity(
     volume1mthUsd = volume1mthUsd,
     rank = rank,
 )
+
+fun ExchangeEntity.toDomain() =
+    Exchange(
+        exchangeId = exchangeId,
+        website = website,
+        name = name,
+        dataQuoteStart = dataQuoteStart?.toPattern(),
+        dataQuoteEnd = dataQuoteEnd?.toPattern(),
+        dataOrderbookStart = dataOrderbookStart?.toPattern(),
+        dataOrderbookEnd = dataOrderbookEnd?.toPattern(),
+        dataTradeStart = dataTradeStart?.toPattern(),
+        dataTradeEnd = dataTradeEnd?.toPattern(),
+        dataSymbolsCount = dataSymbolsCount,
+        volume1hrsUsd = volume1hrsUsd,
+        volume1dayUsd = volume1dayUsd,
+        volume1mthUsd = volume1mthUsd,
+        rank = rank,
+    )
+
+fun String.toPattern(): String {
+    val inputFormatter = DateTimeFormatter.ISO_OFFSET_DATE_TIME
+    val outputFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy - HH:mm:ss")
+
+    return OffsetDateTime.parse(this, inputFormatter).format(outputFormatter)
+}
